a90b982bd4e0901553b3c30086c14111c99819ef
John Behan <behanj@gmail.com>
2016-05-16 09:21:13 +0700

    added tasks updated index.js with function calls

--- README.md
+++ README.md
@@ -2,12 +2,21 @@
 
 Input - png
 
-Output - array of map terrain to file - 
+Output - array of map terrain to file -
 
 ## Usage
-`npm hexMapGenerator.js <input-file.png> <output-file> <max-width> <max-height> <cols> <rows>`
+`npm hexMapGenerator.js <input-file.png> <output-file> <cols> <rows>`
 
 ## Further Development
 - output code for rendering map
 - command line inputs
 - Browser gui
+
+- [ ] get image width and height
+- [ ] get hex radius
+- [ ] get center point of each hex
+- [ ] get pixel sample (array->rgba values) function
+- [ ] get average colour(array->rgb) of each sample
+- [ ] get hsv(array) values from rgb averages
+- [ ] convert hsv to terrain type (array)
+- [ ] output array to file
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 'use strict';
 
-var imageFile = process.argv[2];
-var outputFile = process.argv[3];
-var maxWidth = process.argv[4];
-var maxHeight = process.argv[5];
-var numCols = process.argv[6];
-var numRows = process.argv[7];
+const imageFile = process.argv[2];
+const outputFile = process.argv[3];
+const numCols = process.argv[4];
+const numRows = process.argv[5];
 
-var createGridPoints = require('src/createGridPoints.js');
-var imageWorker = require('src/imageWorker.js');
+const createGridPoints = require('src/createGridPoints.js');
+const imageWorker = require('src/imageWorker.js');
 
-var gridPoints = createGridPoints(maxWidth, maxHeight, numCols, numRows);
+const image = imageWorker(imageFile);
+const gridPoints = createGridPoints(image.width, image.height, numCols, numRows);
